a1d60bf52c054294f77cfcc116982bdc42971a3b
Emilio Sala <emilio_k88@hotmail.com>
2026-03-20 08:06:30 +0100

    ADD: preprocess with stemming

--- cli/search_command.py
+++ cli/search_command.py
@@ -1,21 +1,27 @@
+from nltk.stem import PorterStemmer
 from load_files import load_movies, load_stopwords
 from text_processing import (
-    simple_clean,
-    tokenize_based_word,
+    preprocess,
     compare_list_tokens,
-    remove_stopwords
 )
+
+
 def search(query: str) -> None:
+    """
+    Search for movies based on a query string.
+    
+    Args:
+        query (str): The search query string.
+    """
     print(f"Searching for: {query}")
 
     movies = load_movies()
     stopwords: list[str] = load_stopwords()
+    stemmer = PorterStemmer()
 
     for movie in movies:
-        search_clean = remove_stopwords(simple_clean(query), stopwords)
-        title_clean = remove_stopwords(simple_clean(movie["title"]), stopwords)
-        if compare_list_tokens(
-            tokenize_based_word(search_clean),
-            tokenize_based_word(title_clean)
-        ):
-            print(movie["id"], movie["title"])
+        search_token: list[str] = preprocess(query, stopwords, stemmer)
+        title_token: list[str] = preprocess(movie["title"], stopwords, stemmer)
+
+        if compare_list_tokens(search_token, title_token):
+            print(f"{movie["id"]:<4}: {movie["title"]}")
--- cli/text_processing.py
+++ cli/text_processing.py
@@ -1,4 +1,5 @@
 import string
+from typing import Any
 
 def simple_clean(text: str) -> str:
     table = str.maketrans("", "", string.punctuation)
@@ -12,4 +13,26 @@ def tokenize_based_word(text: str) -> list[str]:
 
 def compare_list_tokens(list1: list[str], list2: list[str]) -> bool:
     set1, set2 = set(list1), set(list2)
-    return set1.intersection(set2)
+    return not set(set1).isdisjoint(set2)
+
+def preprocess(text: str, stopwords: set[str], stemmer: Any) -> list[str]:
+    """
+    Preprocess text by cleaning, removing stopwords, and stemming.
+    
+    Args:
+        text (str): The text to preprocess.
+        stopwords (set[str]): The set of stopwords to remove.
+        stemmer (Any): The stemmer to use.
+    
+    Returns:
+        list[str]: The preprocessed text as a list of tokens.
+    """
+    clean_text = simple_clean(text)
+
+    tokens = [
+        stemmer.stem(word)
+        for word in clean_text.split()
+        if word not in stopwords
+    ]
+
+    return tokens
