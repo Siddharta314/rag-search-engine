5217343e380e4cd56684d0e364e80e9930712a5d
Emilio Sala <emilio_k88@hotmail.com>
2026-04-16 11:05:40 +0200

    ADD: summarize command

--- cli/augmented_generation_cli.py
+++ cli/augmented_generation_cli.py
@@ -1,7 +1,7 @@
 import argparse
 
 from hybrid_search.hybrd_search import HybridSearch
-from hybrid_search.llm_utils import rag_explanation
+from hybrid_search.llm_utils import rag_explanation, rag_summarize
 from load_files import load_movies
 
 
@@ -13,6 +13,17 @@ def main():
         "rag", help="Perform RAG (search + generate answer)"
     )
     rag_parser.add_argument("query", type=str, help="Search query for RAG")
+
+    summarize_parser = subparsers.add_parser("summarize", help="Summarize a document")
+
+    summarize_parser.add_argument("query", type=str, help="Search query for RAG")
+    summarize_parser.add_argument(
+        "--limit",
+        type=int,
+        default=5,
+        help="Limits the number of documents to summarize",
+    )
+
     documents = load_movies()
     hs = HybridSearch(documents)
     args = parser.parse_args()
@@ -33,6 +44,22 @@ def main():
             rag_response = rag_explanation(query, docs)
             print("RAG Response:")
             print(rag_response)
+        case "summarize":
+            query = args.query
+            limit = args.limit
+            results = hs.rrf_search(query, limit)
+            docs = "\n".join(
+                [
+                    f"Title: {doc.get('title', '')}: {doc.get('description', '')}"
+                    for doc in results
+                ]
+            )
+            print("Search Results:")
+            for r in results:
+                print(f"- {r['title']}")
+            summary = rag_summarize(query, docs)
+            print("LLM Summary:")
+            print(summary)
         case _:
             parser.print_help()
 
